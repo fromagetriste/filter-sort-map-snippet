33ace90d117d48710f741e7d59e425e7f3fb1750
unknown <fromagetriste@gmail.com>
2024-07-10 16:54:50 -0600

    second commit

--- src/DessertsList.jsx
+++ src/DessertsList.jsx
@@ -6,12 +6,14 @@ function DessertsList(props) {
     .filter((dessert) => {
       return dessert.calories < 500;
       //   we filter all desserts which are less than 500 cal
+      // note : we condition is true, element is kept in the returned Array, and if element is false, it's filtered out of returned array
     })
     // Maintenant, on va "sort", ranger par ordre croissant, par calorie
     .sort((a, b) => {
       return a.calories - b.calories;
     })
     // Finalement, on va mapper sur la Array et renvoyer des li elements
+    //   WARNING, it's missing a key in my map method although the code still runs
     .map((dessert) => {
       return (
         <li>
